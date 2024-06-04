acaef5ab8306d88b756647f1ee169d2c8fe2d733
frenzie24 <kevincharlesgross@gmail.com>
2024-06-03 22:15:22 -0400

    added comments describing model strctures

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+const {log} = require('@frenzie24/logger');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,6 +11,6 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    log(`API server running on port ${PORT}!`);
   });
 });
